2c191748ccedb3f5f5d07101bbdcdea33de495d2
Jiangzq <sxdxjzq@gmail.com>
2024-04-29 19:23:31 +0800

    Add files via upload

---
+++ preprocessing/source/MinMaxNorm.m
@@ -0,0 +1,26 @@
+function [X] = MinMaxNorm(M, Index)
+%%%% Min-Max Normalization  with incomplete data
+%   Params:
+%   -------
+%       - M: matrix, (sample_num, feature_dim)
+%            Input data matrix.
+%       - Index: matrix, (sample_num, 1)
+%            Missing sample indicator matrix, 1 for exist; 0 for miss.
+
+    if nargin < 2
+        Index = ones(size(M, 1), 1);
+    end
+
+    temp = M(Index, :);
+    temp_max = max(temp);
+    temp_min = min(temp);
+
+    % avoid zero division
+    zero_idx = find((temp_max - temp_min) == 0);
+    temp_max(zero_idx) = temp_max(zero_idx) + 1;
+
+    % min-max normalization
+    X = zeros(size(M));
+    X(Index, :) = (temp - temp_min) ./ (temp_max - temp_min);
+
+end
---
+++ preprocessing/source/ZscoreNorm.m
@@ -0,0 +1,21 @@
+function [X] = ZscoreNorm(M, Index)
+%%%% Z-score Standardization with incomplete data
+%   Params:
+%   -------
+%       - M: matrix, (sample_num, feature_dim)
+%            Input data matrix.
+%       - Index: Indicator matrix, (sample_num, 1)
+%            1 for exists; 0 for missing.
+    if nargin < 2
+        Index = ones(size(M, 1), 1);
+    end
+
+    X = zeros(size(M));
+    temp = M(Index, :);
+    temp_std = std(temp, [], 2);
+    temp_std(temp_std == 0) = 1;
+
+    % Standardization
+    X(Index, :) = (temp - mean(temp, 2)) ./ temp_std;
+
+end
